69567352bc3da6103fc13bac9ef20ce0a2ee2eed
deguchi <deguchi37458@gmail.com>
2024-05-08 16:02:23 +0900

    update

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ import './style.css'
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { PerspectiveCamera } from 'three/src/cameras/PerspectiveCamera';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import GUI from 'lil-gui'; 
 
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+import Lenis from "lenis"
+
+const lenis = new Lenis()
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
 
 // shaderを使用するのであれば下記をコメントアウト
 // import vertexShader from "./shader/vertexShader";
@@ -60,131 +70,128 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Mesh
-const geometry = new THREE.CylinderGeometry( 20, 20, 500, 16 );
-const material = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  wireframe: true,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// ライトの作成
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1.0);
+scene.add(ambientLight);
 
-mesh.position.y = 250
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+directionalLight.position.set(0, 1, 1).normalize();
+scene.add(directionalLight);
 
-const cylinder = new THREE.Group();
-cylinder.add(mesh);
-const cylinderWrap = new THREE.Group();
-cylinderWrap.add(cylinder);
-scene.add(cylinderWrap);
-
-
-// let axis = new THREE.Vector3(0, -1, -1).normalize();
-// let angle = Math.PI / 4;
-// let q = new THREE.Quaternion();
-// q.setFromAxisAngle(axis, angle);
-// mesh.quaternion.copy(q);
-
-// Animate
-cylinder.position.y = -200
-cylinder.position.x = -150
-cylinder.rotation.z = -Math.PI / 5;
-
-// tl01
-const tl01 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.sec01',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    // markers: true,
-  }
-});
-
-tl01
-  .to(cylinder.rotation, {
-    y: Math.PI * 2,
-    z: 0,
-  }, 'tl01')
-  .to(cylinder.position, {
-    x: -300
-  },'tl01')
-
-// tl02
-const tl02 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.sec02',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    // markers: true,
-  }
-});
-
-tl02  
-  .to(cylinder.position, {
-    z: 300,
-    x: -250,
-    y: -100,
-  },'tl02')
-
-
-// tl03
-const tl03 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.sec03',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    // markers: true,
-  }
-});
-
-tl03
-  .to(cylinder.rotation, {
-    y: -Math.PI * 2 ,
-    z: -Math.PI / 5,
-  },'tl03')
-  .to(cylinder.position, {
-    z: 450,
-    x: 100,
-    y: 0,
-  },'tl03')
-
-// tl04
-
-// tl05
-const tl05 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.sec05',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    // markers: true,
-  }
-});
-
-tl05
-  .to(cylinder.position, {
-    x: -300,
-    y: -200,
-  },'tl05')
-
+// GLTF Loader
+const loader = new GLTFLoader();
+loader.load('/_deguchi/3d/pen.glb', function (gltf) {
+  const model = gltf.scene;
+  model.scale.set(50, 50, 50);
+  scene.add(model);
   
-let i = 0;
-const animate = () => {
-  // Update controls
-  controls.update();
+  const modelWrap = new THREE.Group();
+  modelWrap.add(model);
+  scene.add(modelWrap);
   
-  i += 0.015;
-  cylinderWrap.position.y = Math.sin(i) * 5;
-  cylinderWrap.rotation.y = Math.sin(i * 0.01) * 0.5;
+  // model.position.y = -200
+  // model.position.x = -150
+  model.rotation.z = -Math.PI / 5;
 
-  // Render
-  renderer.render(scene, camera);
 
-  window.requestAnimationFrame(animate);
-};
-animate();
+  // Animate
+  // tl01
+  const tl01 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sec01',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      // markers: true,
+    }
+  });
+
+  tl01
+    .to(model.rotation, {
+      y: Math.PI * 2,
+      z: 0,
+    }, 'tl01')
+    .to(model.position, {
+      x: -300
+    },'tl01')
+
+  // tl02
+  const tl02 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sec02',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      // markers: true,
+    }
+  });
+
+  tl02  
+    .to(model.position, {
+      z: 300,
+      x: -250,
+      y: -100,
+    },'tl02')
+
+
+  // tl03
+  const tl03 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sec03',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      // markers: true,
+    }
+  });
+
+  tl03
+    .to(model.rotation, {
+      y: -Math.PI * 2 ,
+      z: -Math.PI / 5,
+    },'tl03')
+    .to(model.position, {
+      z: 450,
+      x: 300,
+      y: 200,
+    },'tl03')
+
+  // tl04
+
+  // tl05
+  const tl05 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sec05',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      // markers: true,
+    }
+  });
+
+  tl05
+    .to(model.position, {
+      x: -300,
+      y: -200,
+    },'tl05')
+
+  let i = 0;
+  const animate = () => {
+    // Update controls
+    controls.update();
+    
+    i += 0.015;
+    modelWrap.position.y = Math.sin(i) * 5;
+    modelWrap.rotation.y = Math.sin(i * 0.01) * 0.5;
+  
+    // Render
+    renderer.render(scene, camera);
+  
+    window.requestAnimationFrame(animate);
+  };
+  animate();
+
+});
 
 
 // Resize
@@ -204,3 +211,6 @@ window.addEventListener("resize", () => {
 
 // const axesHelper = new THREE.AxesHelper( 300 );
 // scene.add( axesHelper );
+
+// const lightHelper = new THREE.SpotLightHelper(directionalLight);
+// scene.add(lightHelper);
